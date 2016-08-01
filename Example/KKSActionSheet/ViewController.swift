a798af5cbece8eb2a482cd51045e418bac339d45
k_krizhanovskii <kk@starladder.tv>
2016-08-01 15:16:09 +0300

    add pod

--- Example/KKSActionSheet/ViewController.swift
+++ Example/KKSActionSheet/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import KKSActionSheet
 
 class ViewController: UIViewController {
 
+    let actionSheet = KKActionSheet()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let button = UIButton(frame: CGRectMake(10, 10, 50, 50));
+        button.addTarget(self, action: #selector(self.press), forControlEvents: .TouchUpInside);
+        button.backgroundColor = .yellowColor();
+        self.view.addSubview(button)
+        
+        
+        
+        
+        
+        actionSheet.actionSheetData = [KKActionSheetData(image: UIImage(),title: "1",complitionHandler:nil),
+                                             KKActionSheetData(image: UIImage(named:"picker_camera"),title:"2",complitionHandler:nil),
+                                             KKActionSheetData(image: UIImage(named:"picker_close"),title: "3",complitionHandler: { Void in
+                                                print("HAndler used")
+                                                button.backgroundColor = .orangeColor();
+                                             })]
+
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +42,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func press() {
+        print("press")
+        
+        actionSheet.showActionSheetWithSender(sender:self)
+    }
 }
 
